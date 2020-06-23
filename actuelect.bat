9f6f83394181f82eeda8b8812bfa1e05c36ae9f1
Walter <dps.github@gmail.com>
2020-06-23 15:19:49 -0300

    DON

---
+++ actuelect.bat
@@ -0,0 +1,8 @@
+XCOPY P:\*.EXE C:\DPS_sql /D /Y 
+XCOPY P:\*.xml C:\DPS_sql /D /Y 
+XCOPY P:\*.pdb C:\DPS_sql /D /Y 
+XCOPY P:\*.dll C:\DPS_sql /D /Y 
+XCOPY P:\*.EXE C:\DPS_sql /D /Y 
+XCOPY P:\*.config C:\DPS_sql /D /Y 
+
+pause
